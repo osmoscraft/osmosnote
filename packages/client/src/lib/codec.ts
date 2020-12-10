e1262df3dbce3957ca8bd7317d9878f7d78f85d6
Chuanqi Sun <chuanqi-no-reply@gmail.com>
2020-12-09 21:16:27 -0800

    encode/decode headings

--- packages/client/src/lib/codec.ts
+++ packages/client/src/lib/codec.ts
@@ -1,7 +1,7 @@
 export function markdownToHtml(markdown: string): string {
   const html = markdown
     .split("\n")
-    .map((line, i) => `<pre data-line="${i}">${markdownLineToHtmlLine(line)}</pre>`)
+    .map((line, i) => `<div data-line="${i}">${markdownLineToHtmlLine(line)}</div>`)
     .join("");
 
   return html;
@@ -10,7 +10,7 @@ export function markdownToHtml(markdown: string): string {
 export function domToMarkdown(dom: HTMLElement): string {
   const domCloned = dom.cloneNode(true) as HTMLElement;
 
-  const markdown = [...domCloned.querySelectorAll(`pre[data-line]`)]
+  const markdown = [...domCloned.querySelectorAll(`div[data-line]`)]
     .map((domLine) => DomLineToMarkdownLine(domLine as HTMLElement))
     .join("\n");
 
@@ -18,19 +18,44 @@ export function domToMarkdown(dom: HTMLElement): string {
 }
 
 function markdownLineToHtmlLine(markdown: string): string {
-  const INTERNAL_LINK_REGEX = /\[([^\(]+)\]\(([^\[]\d+)\)/g; // e.g. [Some title](200012300630)
-
-  function replacer(_match: string, title: string, id: string) {
-    return `<a data-id="${id}" href="/editor.html?filename=${encodeURIComponent(
-      `${id}.md`
-    )}" contenteditable="false">${title}</a>`;
-  }
-
-  return markdown.replace(INTERNAL_LINK_REGEX, replacer);
+  return markdown
+    .replace(S2Heading.MARKDOWN_REGEX, S2Heading.MARKDOWN_REPLACER)
+    .replace(S2Link.MARKDOWN_REGEX, S2Link.MARKDOWN_REPLACER);
 }
 
 function DomLineToMarkdownLine(dom: HTMLElement): string {
-  dom.querySelectorAll(`a`).forEach((anchor) => (anchor.outerHTML = `[${anchor.innerText}](${anchor.dataset.id})`));
+  dom.querySelectorAll(S2Link.DOM_SELECTOR).forEach(S2Link.DOM_REPLACER);
+  dom.querySelectorAll(S2Heading.DOM_SELECTOR).forEach(S2Heading.DOM_REPLACER);
 
   return dom.innerText;
 }
+
+class S2Heading extends HTMLHeadingElement {
+  static DOM_SELECTOR = `h1[^s2-heading], h2[^s2-heading], h3[^s2-heading], h4[^s2-heading], h5[^s2-heading], h6[^s2-heading]`;
+  static DOM_REPLACER = (element: Element) => (element.outerHTML = (element as S2Heading).markdownText);
+
+  static MARKDOWN_REGEX = /^#{1,6} (.*)/gm; // e.g. # My title
+  static MARKDOWN_REPLACER = (_match: string, title: string) => {
+    const level = _match.split(" ")[0].length;
+    return `<h${level} is="s2-heading" data-level="${level}">${"#".repeat(level)} ${title}</h${level}>`;
+  };
+
+  get markdownText() {
+    return this.innerText;
+  }
+}
+customElements.define("s2-heading", S2Heading, { extends: "h1" });
+
+class S2Link extends HTMLAnchorElement {
+  static DOM_SELECTOR = `a[is="s2-link"]`;
+  static DOM_REPLACER = (element: Element) => (element.outerHTML = (element as S2Link).markdownText);
+
+  static MARKDOWN_REGEX = /\[([^\(]+)\]\(([^\[]\d+)\)/g; // e.g. [Some title](200012300630)
+  static MARKDOWN_REPLACER = (_match: string, title: string, id: string) =>
+    `<a is="s2-link" data-id="${id}" href="/editor.html?filename=${encodeURIComponent(`${id}.md`)}">${title}</a>`;
+
+  get markdownText() {
+    return `[${this.innerText}](${this.dataset.id ?? this.getAttribute("href")})`;
+  }
+}
+customElements.define("s2-link", S2Link, { extends: "a" });
