5eb0ded5cbf74b101ed16bb96984ad573d539268
Chuanqi Sun <chuanqi-no-reply@gmail.com>
2020-12-31 13:42:32 -0800

    remove unused code

--- packages/client/src/components/text-editor/overlay/parts/link-overlay.component.ts
+++ packages/client/src/components/text-editor/overlay/parts/link-overlay.component.ts
@@ -7,7 +7,6 @@ import "./link-overlay.css";
 
 export class LinkOverlayComponent extends HTMLElement {
   private cursorSelectionService!: CursorSelectionService;
-  private anchorDom!: HTMLAnchorElement;
 
   readonly dataset!: {
     title: string;
@@ -15,19 +14,8 @@ export class LinkOverlayComponent extends HTMLElement {
     cursorIn?: "on";
   };
 
-  constructor() {
-    super();
-
-    // this.handleCursorSelectionEvent = this.handleCursorSelectionEvent.bind(this);
-    // this.handleOpenLink = this.handleOpenLink.bind(this);
-  }
-
   connectedCallback() {
     this.cursorSelectionService = di.getSingleton(CursorSelectionService);
-    // this.cursorSelectionService.eventTarget.addEventListener(
-    //   "cursor-selection:change",
-    //   this.handleCursorSelectionEvent
-    // );
 
     this.innerHTML = /*html*/ `<code class="s2-link-overlay__symbol">[</code><code class="s2-link-overlay__title">${
       this.dataset.title
@@ -35,42 +23,16 @@ export class LinkOverlayComponent extends HTMLElement {
       `${this.dataset.id}.md`
     )}">${this.dataset.id}</a><code class="s2-link-overlay__symbol">)</code>`;
 
-    this.anchorDom = this.querySelector("a")!;
-
     this.handleCursorSelection(this.cursorSelectionService.getCurrentSelection());
   }
-
-  // disconnectedCallback() {
-  //   this.cursorSelectionService.eventTarget.removeEventListener(
-  //     "cursor-selection:change",
-  //     this.handleCursorSelectionEvent
-  //   );
-  //   window.removeEventListener("keydown", this.handleOpenLink);
-  // }
-
-  // private handleCursorSelectionEvent(e: CustomEvent<CursorSelection>) {
-  //   this.handleCursorSelection(e.detail);
-  // }
 
   private handleCursorSelection(selection: CursorSelection) {
     if (!this.dataset.cursorIn && this.dataset.id === selection.linkId) {
       // off to on
       this.dataset.cursorIn = "on";
-      // window.addEventListener("keydown", this.handleOpenLink);
     } else if (this.dataset.cursorIn === "on" && this.dataset.id !== selection.linkId) {
       // on to off
       delete this.dataset.cursorIn;
-      // window.removeEventListener("keydown", this.handleOpenLink);
     }
   }
-
-  // private handleOpenLink(event: KeyboardEvent) {
-  //   if (event.key === "Enter") {
-  //     if (this.dataset.cursorIn === "on") {
-  //       event.preventDefault();
-  //       event.stopPropagation();
-  //       window.open(this.anchorDom.href, event.ctrlKey ? undefined : "_self");
-  //     }
-  //   }
-  // }
 }
