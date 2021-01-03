d50b1355ea8f484150ae513a57cf40eee0483847
Chuanqi Sun <chuanqi-no-reply@gmail.com>
2021-01-02 19:18:06 -0800

    fix hanging listener issue

--- packages/client/src/services/window-bridge/window-bridge.service.ts
+++ packages/client/src/services/window-bridge/window-bridge.service.ts
@@ -13,10 +13,10 @@ declare global {
 }
 
 export class WindowBridgeService {
-  private activeChildNoteCreatedHandler: ((e: any) => void) | null = null;
-  private activeCancelHandler: ((e: any) => void) | null = null;
-
-  constructor(private componentRefs: ComponentReferenceService) {}
+  constructor(private componentRefs: ComponentReferenceService) {
+    this.handleCancel = this.handleCancel.bind(this);
+    this.handleChildNoteCreated = this.handleChildNoteCreated.bind(this);
+  }
 
   insertNoteLinkAfterCreated(openUrl: string) {
     // we can handle one action at a time, so stop handling any previous action
@@ -24,30 +24,8 @@ export class WindowBridgeService {
 
     this.componentRefs.statusBar.setMessage("Save child note to insert, [ESC] to cancel", "warning");
 
-    const handleChildNoteCreated = (ev: CustomEvent<NoteCreatedDetail>) => {
-      const insertion = `[${ev.detail.title}](${ev.detail.id})`;
-
-      this.componentRefs.textEditor.insertAtCursor(insertion);
-      this.componentRefs.statusBar.setMessage(`Inserted: "${insertion}"`);
-      this.cancelInsertLinkOnSave();
-    };
-
-    const handleCancel = (e: KeyboardEvent) => {
-      if (e.key === "Escape") {
-        this.cancelInsertLinkOnSave();
-        this.componentRefs.statusBar.setMessage(`Cancelled`);
-        console.log("[command-bar] cancelled handling child note created");
-
-        e.preventDefault();
-        e.stopPropagation();
-      }
-    };
-
-    this.activeChildNoteCreatedHandler = handleChildNoteCreated;
-    this.activeCancelHandler = handleCancel;
-
-    window.addEventListener("command-bar:child-note-created", handleChildNoteCreated);
-    window.addEventListener("keydown", handleCancel, { capture: true });
+    window.addEventListener("command-bar:child-note-created", this.handleChildNoteCreated);
+    window.addEventListener("keydown", this.handleCancel, { capture: true });
 
     window.open(openUrl);
   }
@@ -64,15 +42,27 @@ export class WindowBridgeService {
     }
   }
 
-  private cancelInsertLinkOnSave() {
-    if (this.activeCancelHandler) {
-      window.removeEventListener("keydown", this.activeCancelHandler);
-      this.activeCancelHandler = null;
-    }
+  private handleChildNoteCreated(ev: CustomEvent<NoteCreatedDetail>) {
+    const insertion = `[${ev.detail.title}](${ev.detail.id})`;
 
-    if (this.activeChildNoteCreatedHandler) {
-      window.removeEventListener("command-bar:child-note-created", this.activeChildNoteCreatedHandler);
-      this.activeChildNoteCreatedHandler = null;
+    this.componentRefs.textEditor.insertAtCursor(insertion);
+    this.componentRefs.statusBar.setMessage(`Inserted: "${insertion}"`);
+    this.cancelInsertLinkOnSave();
+  }
+
+  private handleCancel(e: KeyboardEvent) {
+    if (e.key === "Escape") {
+      this.cancelInsertLinkOnSave();
+      this.componentRefs.statusBar.setMessage(`Cancelled`);
+      console.log("[command-bar] cancelled handling child note created");
+
+      e.preventDefault();
+      e.stopPropagation();
     }
+  }
+
+  private cancelInsertLinkOnSave() {
+    window.removeEventListener("keydown", this.handleCancel, { capture: true });
+    window.removeEventListener("command-bar:child-note-created", this.handleChildNoteCreated);
   }
 }
