d84829519d6d2dc0264117ea0a08e93f62df18e2
Chuanqi Sun <chuanqi-no-reply@gmail.com>
2021-04-04 23:22:46 -0700

    expand to full line when restore non-collapsed cursor

--- packages/server/public/src/components/text-editor/compiler/compile.service.ts
+++ packages/server/public/src/components/text-editor/compiler/compile.service.ts
@@ -1,13 +1,12 @@
 import { sanitizeHtml } from "../../../utils/sanitize-html.js";
 import type { CaretService } from "../caret.service.js";
+import type { LineElement } from "../helpers/source-to-lines.js";
+import type { LineQueryService } from "../line-query.service.js";
 import { blank } from "./blank.js";
 import { generic } from "./generic.js";
 import { heading } from "./heading.js";
 import { list } from "./list.js";
 import { meta } from "./meta.js";
-import type { LineElement, LineType } from "../helpers/source-to-lines.js";
-import type { LineQueryService } from "../line-query.service.js";
-import { SRC_LINE_END } from "../../../utils/special-characters.js";
 
 export interface FormatContext {
   indentFromHeading: number;
@@ -55,15 +54,32 @@ export class CompileService {
    * Parse raw text, format, and update cursor position
    */
   compile(root: HTMLElement | DocumentFragment, config: CompileConfig = {}) {
-    let caretLine: HTMLElement;
-    let previousCaretOffset: number | null = null;
+    let caretAnchorLine: HTMLElement;
+    let caretFocusLine: HTMLElement;
+    let previousCaretAnchorOffset: number | null = null;
+    let previousCaretFocusOffset: number | null = null;
+    let isCollapsed = false;
+    let isInverted = false;
+    let formatResult!: FormatResult | null;
+    let shouldRestore = false;
+
+    // TODO until we can tracks size change at arbitrary position, the cursor restore only works for indent change for collapsed cursor
+    // Consider insert a bookmark element in dom just before compiling to track cusor position.
+    // Consider asking each line compiler to export a size change map {from: [start, end], to: [newStart, newEnd]}[]
 
     if (!config.noCursorRestore) {
       const caret = this.caretService.caret;
       if (caret) {
-        caretLine = this.lineQueryService.getLine(caret.focus.node)!;
-        const { offset } = this.caretService.getCaretLinePosition(caret.focus);
-        previousCaretOffset = offset;
+        shouldRestore = true;
+        isCollapsed = caret.isCollapsed;
+        isInverted = caret.anchor.node !== caret.start.node;
+        caretFocusLine = this.lineQueryService.getLine(caret.focus.node)!;
+        caretAnchorLine = this.lineQueryService.getLine(caret.anchor.node)!;
+        const { offset: anchorOffset } = this.caretService.getCaretLinePosition(caret.focus);
+        previousCaretAnchorOffset = anchorOffset;
+
+        const { offset: focusOffset } = this.caretService.getCaretLinePosition(caret.anchor);
+        previousCaretFocusOffset = focusOffset;
       }
     }
 
@@ -89,23 +105,40 @@ export class CompileService {
           line.dataset.parsed = "";
 
           // 2. format
-          const formatLineSummary = config.noFormatting ? null : lineCompiler.format(line, context);
+          const result = config.noFormatting ? null : lineCompiler.format(line, context);
+          if (line === caretAnchorLine) {
+            formatResult = result;
+          }
 
           // 3. context update
           lineCompiler.updateContext?.(line, context);
-
-          // 4. restore cursor
-          if (!config.noCursorRestore && (line as any) === caretLine) {
-            const newOffset = Math.max(0, previousCaretOffset! + (formatLineSummary?.lengthChange ?? 0));
-            this.caretService.setCollapsedCaretToLineOffset({ line: caretLine, offset: newOffset });
-          }
-
           return;
         }
       }
 
       console.error(`[format] fatal error. Unexpected token on line ${index}. Line text: ${line.textContent}`);
     });
+
+    // 4. restore cursor
+    if (shouldRestore) {
+      if (isCollapsed) {
+        if (formatResult) {
+          const newOffset = Math.max(0, previousCaretAnchorOffset! + (formatResult.lengthChange ?? 0));
+          this.caretService.setCollapsedCaretToLineOffset({ line: caretFocusLine!, offset: newOffset });
+        }
+      } else {
+        // expand to cover all active lines
+        // TODO improve this to retain as much selection as possible
+        this.caretService.setCaretsBySeekOutput({
+          anchor: isInverted
+            ? this.lineQueryService.seekToLineEnd(caretAnchorLine!)
+            : this.lineQueryService.seekToLineStart(caretAnchorLine!),
+          focus: isInverted
+            ? this.lineQueryService.seekToLineStart(caretFocusLine!)
+            : this.lineQueryService.seekToLineEnd(caretFocusLine!),
+        });
+      }
+    }
   }
 
   getPortableText(lines: HTMLElement[], startLineOffset = 0, endLineOffset?: number): string {
