1bf0fac511d4849e19e42bbd47c7643703c3a60a
Chuánqí Sun <1895289+chuanqisun@users.noreply.github.com>
2021-11-10 18:26:19 -0800

    Update install.sh

--- packages/scripts/install.sh
+++ packages/scripts/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget -O ripgrep.deb https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+wget -O ripgrep.deb https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_12.1.1_amd64.deb
 dpkg -i ripgrep.deb
