cfaf3da1a12d9435d7bd42374a182a6e56a7a744
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2023-06-15 23:55:49 -0700

    prepare ast for linting

--- packages/tools/markdown-converter/haiku-to-md.js
+++ packages/tools/markdown-converter/haiku-to-md.js
@@ -2,6 +2,7 @@ const path = require("path");
 const fs = require("fs/promises");
 const assert = require("assert");
 const inputDir = path.resolve(__dirname, "./input");
+const yaml = require("yaml");
 
 console.log(inputDir);
 
@@ -13,7 +14,7 @@ async function main(inputDir) {
 
   fs.mkdir(path.resolve(__dirname, "./output"), { recursive: true });
   const haikuFiles = await fs.readdir(inputDir);
-  console.log(haikuFiles);
+  console.log("file count: ", haikuFiles.length);
 
   for (const haikuFile of haikuFiles) {
     await fs.readFile(path.resolve(inputDir, haikuFile), "utf-8").then(async (data) => {
@@ -31,79 +32,171 @@ async function main(inputDir) {
       }
 
       // convert headerLines to yaml
-      const frontmatterLines = headerLines.map((line) => {
-        let [_, key, value] = line.match(/^#\+(.*):\s(.*)$/);
-
-        assert(!value.includes(`"`), `${value} contains double quotes`);
-        if (value.includes(`: `)) {
-          value = `"${value}"`;
-        }
-
-        if (key === "tags") {
-          return `${key}: [${value
-            .split(",")
-            .map((tag) => tag.trim())
-            .filter(Boolean)
-            .join(", ")}]`;
-        }
-        return `${key}: ${value}`;
-      });
-
-      const frontmatter = `---\n${frontmatterLines.join("\n")}\n---\n`;
-
-      // trim bodyLines
-      while (bodyLines[0]?.trim() === "") {
-        bodyLines.shift();
-      }
-      while (bodyLines.at(-1)?.trim() === "") {
-        bodyLines.pop();
-      }
-
-      // convert bodyLines to markdown lines
-      const markdownLines = bodyLines.map((line) => {
-        if (line.startsWith("#")) {
-          // convert title as is
-          return line;
-        } else if (line.match(/^(-+\d\.) (.*)$/)) {
-          const [_, prefix, digit, text] = line.match(/^(-*)(\d+\.) (.*)$/);
-          const order = parseInt(digit);
-          assert(typeof order === "number", `${line} has non-numeric ordered list item ${_}`);
-          assert(order < 10, `${line} has two digit ordered list item ${_}`);
-          return `${"   ".repeat(prefix.length)}${digit}. ${text}`;
-        } else if (line.match(/^(-+) (.*)$/)) {
-          const [_, prefix, text] = line.match(/^(-+) (.*)$/);
-          return `${"  ".repeat(prefix.length - 1)}- ${text}`;
-        }
-
-        // multiline quotes
-
-        // relative link url
-
-        // TODO Add two spaces to the line before paragraph lines
-
-        return line;
-      });
-
-      const bodyText = markdownLines.join("\n");
-
-      // console.log(haikuFile);
-      // console.log(frontmatter);
-      // console.log(bodyText);
-
-      assert(frontmatterLines.length > 0, `${haikuFile} has no frontmatter`);
-
-      if (!bodyText.length) {
-        console.warn(`${haikuFile} has no body text`);
-      }
+      const frontmatter = handleHeaderLines(haikuFile, headerLines);
+      const body = handleBodyLines(haikuFile, bodyLines);
 
       // TODO pass through markdown and yaml parser
 
       await fs.writeFile(
         path.join(__dirname, `./output/${haikuFile.replace(".haiku", ".md")}`),
-        `${frontmatter}\n${bodyText}\n`
+        `${frontmatter}\n${body}\n`
       );
     });
   }
+}
+
+/**
+ * @param {string[]} headerLines
+ */
+function handleHeaderLines(haikuFile, headerLines) {
+  const frontmatterLines = headerLines.map((line) => {
+    let [_, key, value] = line.match(/^#\+(.+?):\s(.*)$/);
+
+    assert(!value.includes(`"`), `${value} contains double quotes`);
+    if (value.includes(`: `)) {
+      console.warn(`Title "${value}" contains colon`);
+      value = `"${value}"`;
+    }
+
+    if (key === "tags") {
+      return `${key}: [${value
+        .split(",")
+        .map((tag) => tag.trim())
+        .filter(Boolean)
+        .join(", ")}]`;
+    }
+    return `${key}: ${value}`;
+  });
+
+  assert(frontmatterLines.length > 0, `${haikuFile} has no frontmatter`);
+
+  const innerYaml = frontmatterLines.join("\n");
+  let parsedYaml = {};
+  try {
+    parsedYaml = yaml.parse(innerYaml);
+    if (parsedYaml.tags) {
+      assert(Array.isArray(parsedYaml.tags), `${haikuFile} has non-array tags`);
+      parsedYaml.tags.every((tag) => assert(typeof tag === "string", `${haikuFile} has non-string tag`));
+    }
+
+    if (parsedYaml.url) {
+      assert(typeof parsedYaml.url === "string", `${haikuFile} has non-string url`);
+      try {
+        new URL(parsedYaml.url);
+      } catch (e) {
+        console.log(`${haikuFile} has invalid url`);
+        throw e;
+      }
+    }
+  } catch (e) {
+    console.log(`${haikuFile} has invalid yaml`);
+    throw e;
+  }
+  assert(parsedYaml.title, `${haikuFile} has no title`);
+
+  const frontmatter = `---\n${frontmatterLines.join("\n")}\n---\n`;
+
+  return frontmatter;
+}
+
+/**
+ * @param {string[]} bodyLines
+ */
+function handleBodyLines(haikuFile, bodyLines) {
+  // trim bodyLines
+  while (bodyLines[0]?.trim() === "") {
+    bodyLines.shift();
+  }
+  while (bodyLines.at(-1)?.trim() === "") {
+    bodyLines.pop();
+  }
+
+  // 1st pass, annotate line with types
+  const patterns = [
+    [/^#+ /, "HEADING"],
+    [/^(-*)(\d+)\. (.*)$/, "ORDERED_LIST_ITEM"],
+    [/^(-+) (.*)$/, "UNORDERED_LIST_ITEM"],
+    [/^> (.*)$/, "QUOTE"],
+    [/^(\s)*$/, "BLANK_LINE"],
+    [/^.+$/, "PARAGRAPH"],
+  ];
+
+  const astLines = bodyLines.map((line) => {
+    const pattern = patterns.find(([pattern]) => line.match(pattern));
+    assert(pattern, `${haikuFile} "${line}" has no matching pattern`);
+    const match = line.match(pattern[0]);
+
+    switch (pattern[1]) {
+      case "HEADING": {
+        return {
+          type: "HEADING",
+          line,
+        };
+      }
+      case "ORDERED_LIST_ITEM": {
+        const [_, prefix, digit, text] = match;
+        const order = parseInt(digit);
+        const depth = prefix.length;
+
+        assert(typeof order === "number", `${haikuFile} "${line}" has non-numeric ordered list item ${_}`);
+        assert(text.trim().length > 0, `${haikuFile} "${line}" has empty ordered list item text`);
+
+        return {
+          type: "ORDERED_LIST_ITEM",
+          order,
+          depth,
+          line: `${"  ".repeat(depth)}- ${digit}. ${text}`,
+        };
+      }
+      case "UNORDERED_LIST_ITEM": {
+        const [_, prefix, text] = match;
+        const depth = prefix.length - 1;
+
+        assert(text.trim().length > 0, `${haikuFile} "${line}" has empty unordered list item text`);
+
+        return {
+          type: "UNORDERED_LIST_ITEM",
+          depth,
+          line: `${"  ".repeat(depth)}- ${text}`,
+        };
+      }
+      case "QUOTE": {
+        return {
+          type: "QUOTE",
+          line,
+        };
+      }
+      case "BLANK_LINE": {
+        assert(line.length === 0, `${haikuFile} "${line}" has non-empty blank line`);
+        return {
+          type: "BLANK_LINE",
+          line: "",
+        };
+      }
+      case "PARAGRAPH": {
+        return {
+          type: "PARAGRAPH",
+          line: line.trim(),
+        };
+      }
+    }
+  });
+
+  // TODO inter-line lint
+  // multiline quotes
+  // relative link url
+  // TODO Add two spaces to the line before paragraph lines
+  // increment of list item depth must be 1
+  // ordered list cannot be nested under unordered list
+  const markdownLines = astLines.map((astLine) => astLine.line);
+
+  const bodyText = markdownLines.join("\n");
+
+  if (!bodyText.length) {
+    console.warn(`${haikuFile} has no body text`);
+  }
+
+  return bodyText;
 }
 
 main(inputDir);
